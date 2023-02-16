f52f9d5cb08aef947403467c7a91cdccdd2cc1b8
Iader E. Garcia <iader.garcia@alternova.com>
2023-02-15 19:10:24 -0500

    fix: public folder fix: package.json fix: imports fix: url to fetch test

--- netlify/functions/swap-token/swap-token.js
+++ netlify/functions/swap-token/swap-token.js
@@ -1,26 +1,28 @@
+
 import fetch from 'node-fetch';
-import {
-  spotifyClientId,
-  spotifyClientSecret,
-  spotifyClientCallback,
-  spotifyEndPoint,
-  grantRefreshType
-} from '../../../constants';
+import dotenv from 'dotenv';
+const spotifyClientId = process.env.SPOTIFY_CLIENT_ID;
+const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+const spotifyClientCallback = process.env.SPOTIFY_CLIENT_CALLBACK;
+const spotifyEndPoint = process.env.SPOTIFY_END_POINT;
+const grantRefreshType = 'refresh_token';
 
-
-const handler = async (event) => {
+export const handler = async (event) => {
   try {
     console.log('Swap Token');
+
+    const spotifyCode = JSON.parse(event.body).code
 
     const authOptions = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
-      body: `grant_type=${grantRefreshType}&refresh_token=${refresh_token}&client_id=${spotifyClientId}&client_secret=${spotifyClientSecret}&redirect_uri=${spotifyClientCallback}`
+      body: `grant_type=${grantRefreshType}&refresh_token=${spotifyCode}&client_id=${spotifyClientId}&client_secret=${spotifyClientSecret}&redirect_uri=${spotifyClientCallback}`
     }
 
     const result = fetch(spotifyEndPoint, authOptions);
+    console.log(result);
 
     return {
       statusCode: 200,
@@ -33,4 +35,3 @@ const handler = async (event) => {
   }
 }
 
-module.exports = { handler }
